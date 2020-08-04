070224800d7f28818cb38b448a3e24b49f6da0d5
i_study <istudy@grigoris-mbp.attlocal.net>
2020-08-03 22:44:37 -0700

    commit 4

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name="base-home"),
     path('about', views.about, name="base-about"),
     path('services', views.service, name="base-services"),
-    path('pricing', views.pricing, name="base-pricing"),
     path('contact', views.contacts, name="base-contacts"),
+    path('submit', views.send_email, name="base-sendEmail")
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from nylas import APIClient
+import json
+
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+# set up nylas client
+print(config.get('CLIENT_ID'))
+nylas = APIClient(config.get('CLIENT_ID'), config.get('CLIENT_SECRET'), config.get('ACCESS_TOKEN'))
 
 
 def home(request):
@@ -13,9 +22,26 @@ def service(request):
     return render(request, 'base/template.html')
 
 
-def pricing(request):
-    return render(request, 'base/template.html')
-
-
 def contacts(request):
     return render(request, 'base/template.html')
+
+
+def send_email(request):
+    # send the email from a get request
+    draft = nylas.drafts.create()
+
+    # configure the draft
+    draft.subject = "FORM REQUEST"
+    draft.body = "This email content should be the description of the user order including their email anf info \n" \
+                 " along with order information"
+    draft.to = [{'name': 'Michael', 'email': 'creeperarcade2.0@gmail.com'}]
+
+    try:
+        # attempt to send an email
+        draft.send()
+        print("Success")
+    except:
+        # email failed to send for some reason
+        print('Fail')
+
+    return redirect('base-home')
